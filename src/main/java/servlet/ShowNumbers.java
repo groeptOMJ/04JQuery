53f140c13dc05cd71ca87e8c1ea94ff36aef31fe
Stijn <Stijn.heylen@groept.be>
2015-03-23 22:13:45 +0100

    Update - use jdk 1.8

--- src/main/java/servlet/ShowNumbers.java
+++ src/main/java/servlet/ShowNumbers.java
@@ -9,21 +9,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** MVC-based servlet to return some random numbers and colors.
+/**
+ * MVC-based servlet to return some random numbers and colors.
  */
 @WebServlet("/show-nums")
 public class ShowNumbers extends HttpServlet {
-  @Override
-  public void doGet(HttpServletRequest request,
-                    HttpServletResponse response)
-      throws ServletException, IOException {
-    response.setHeader("Cache-Control", "no-cache");
-    response.setHeader("Pragma", "no-cache");
-    double[] nums = { Math.random(), Math.random(), Math.random() };
-    request.setAttribute("nums", nums);
-    response.setContentType("application/json");
-    String outputPage = "/WEB-INF/results/show-nums.jsp";
-    RequestDispatcher dispatcher = request.getRequestDispatcher(outputPage);
-    dispatcher.include(request, response);
-  }
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setHeader("Cache-Control", "no-cache");
+		response.setHeader("Pragma", "no-cache");
+		double[] nums = { Math.random(), Math.random(), Math.random() };
+		request.setAttribute("nums", nums);
+		response.setContentType("application/json");
+		String outputPage = "/WEB-INF/results/show-nums.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(outputPage);
+		dispatcher.include(request, response);
+	}
 }

e6667abc69010aebb884dcac55a804646341c663
Stijn <Stijn.heylen@groept.be>
2015-03-24 02:08:46 +0100

    Added example validation

--- src/main/webapp/scripts/jquery-ajaxcalls2.js
+++ src/main/webapp/scripts/jquery-ajaxcalls2.js
@@ -8,6 +8,27 @@ $(function() {
     		}
     	}
     );
+    $("#data-form2").validate( {
+    	debug: true,
+    	rules: {
+    		id:{
+    			required : true,
+    			minlength : 2
+    		} 
+	    },
+	    messages: {
+	        id:{
+	        	required : "Please enter your id",
+    			minlength : "Minimum length is 2"
+	        } 
+	    }
+    });
+    $("#button-2").click(function(){    	
+		if($("#data-form2").valid()){
+			showName2();
+		}
+	}
+);
 });
 
 //ajax call to show-time jsp, passing the result to showAlert
@@ -22,6 +43,15 @@ function showName() {
            cache: false 
            });
 }
+function showName2() {
+	  $.ajax({ url: "search-name",
+		  	   data: $("#data-form2").serialize(),
+		  	   dataType : "json",
+	           success: insertResult,
+	           error: function(){alert("error");},
+	           cache: false 
+	           });
+	}
 
 //send form data to jsp and load result into result region
 function insertResult(json) {
